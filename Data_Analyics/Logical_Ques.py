0d55d5ea1242458baab00985a6399f1ac1fdbb08
AmiT-Gupta7 <guptaamit61739@gmail.com>
2025-09-15 16:28:44 +0530

    Pandas Functions: groupby(),first(),last() etc

---
+++ Data_Analyics/Logical_Ques.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number: "))
+ap = 2 ** (n-1)
+print("The answer is:", ap)
+
+## sum
+
+r = 2
+a = 2
+sum = a*((r**n-1)/(r-1))
+print("The sum is :",sum)
---
+++ Data_Analyics/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ap = 2 ** (n-1)
+# print("The answer is:", ap)
